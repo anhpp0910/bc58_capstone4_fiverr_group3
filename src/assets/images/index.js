0002ed4c802264a63748d9abf3d3a8bed17bd570
anhpp0910 <phuonganhphanhhh@gmail.com>
2024-02-19 10:12:48 +0700

    change noAvatar img

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,7 +2,7 @@ const images = {
     darkLogoHeader: require('./darkLogoHeader.svg').default,
     lightLogoHeader: require('./lightLogoHeader.svg').default,
     logoFooter: require('./logoFooter.svg').default,
-    noAvatar: require('./noAvatar.jpeg'),
+    noAvatar: require('./noAvatar.png'),
 };
 
 export default images;
